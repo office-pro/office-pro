6187ea6d94271c1fdca60643611a4e4a842bee5e
office-pro <shaelm29@gmail.com>
2016-12-17 11:20:31 +0530

    my project

---
+++ office-pro/src/main.ts
@@ -0,0 +1,16 @@
+import './polyfills.ts';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
+import { AppModule } from './app/';
+import { MemberModule } from './Member-List/Member.module';
+
+
+if (environment.production) {
+  enableProdMode();
+}
+
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(MemberModule);
