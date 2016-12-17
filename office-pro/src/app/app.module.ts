71e17798f0f387806147373232ac383fcdb78ce0
office-pro <shaelm29@gmail.com>
2016-12-17 16:13:30 +0530

    router angular

--- office-pro/src/app/app.module.ts
+++ office-pro/src/app/app.module.ts
@@ -3,18 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import { routes } from './app.router';
 
 import { AppComponent } from './app.component';
+import { ServicesComponent } from './services/services.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServicesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
