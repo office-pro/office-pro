be4b461769fac315996e7862cb7832b257521c93
office-pro <shaelm29@gmail.com>
2017-01-23 00:43:24 +0530

    commit date 21 jan 12:43 am

--- office-pro/src/app/app.component.ts
+++ office-pro/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { TaskService } from './task.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'app works!';
@@ -14,4 +14,19 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     
   }
+   selected = '';
+  items = [
+    {text: 'Refresh'},
+    {text: 'Settings'},
+    {text: 'Help', disabled: true},
+    {text: 'Sign Out'}
+  ];
+
+  iconItems = [
+    {text: 'Redial', icon: 'dialpad'},
+    {text: 'Check voicemail', icon: 'voicemail', disabled: true},
+    {text: 'Disable alerts', icon: 'notifications_off'}
+  ];
+
+  select(text: string) { this.selected = text; }
 }
