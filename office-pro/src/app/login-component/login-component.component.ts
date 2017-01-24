f5cde0bee46796e901c37810497fec4eade7e8f2
office-pro <shaelm29@gmail.com>
2017-01-24 21:46:17 +0530

    aaa

--- office-pro/src/app/login-component/login-component.component.ts
+++ office-pro/src/app/login-component/login-component.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Login } from './login';
 @Component({
   selector: 'app-login-component',
   templateUrl: './login-component.component.html',
   styleUrls: ['./login-component.component.css']
 })
 export class LoginComponentComponent implements OnInit {
+  username:string;
+  password:string;
+  constructor(private login:Login) { 
 
-  constructor() { }
+  }
+
+  Submit() :void{
+    alert("I am in submit "+ this.username);
+    this.login.userName = this.username;
+    this.login.password = this.password;
+    alert(JSON.stringify(this.login))
+  }
 
   ngOnInit() {
   }
