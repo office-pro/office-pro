bc8a4daab761cda24894d2b89ee2ad4c27db7edb
office-pro <shaelm29@gmail.com>
2017-01-26 00:19:57 +0530

    commit on signup ui

--- office-pro/src/app/login-component/login-component.component.ts
+++ office-pro/src/app/login-component/login-component.component.ts
@@ -1,22 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Login } from './login';
 @Component({
   selector: 'app-login-component',
   templateUrl: './login-component.component.html',
   styleUrls: ['./login-component.component.css']
 })
 export class LoginComponentComponent implements OnInit {
-  username:string;
-  password:string;
-  login:Login = new Login();
-  constructor() { 
-
-  }
+  private login= {};
+  constructor() { }
 
   Submit() :void{
-    alert("I am in submit "+ this.username);
-    this.login.userName = this.username;
-    this.login.password = this.password;
     alert(JSON.stringify(this.login))
   }
 
