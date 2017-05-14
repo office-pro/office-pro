814df285fe83302c78453c2e771540d78321345a
office-pro <shaelm29@gmail.com>
2017-05-14 16:07:30 +0530

    'changes'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,37 @@
-import { Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, trigger, state, 
+          style, transition, animate } from '@angular/core';
 import { Abouts } from './about/abouts';
 import { TaskService } from './task.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+     trigger('flyOut', [
+       state('inactive', style({
+         backgroundColor: '#eee'
+       })),
+       state('active',style({
+         backgroundColor: '#cfd8dc'
+       })),
+      transition("inactive <=> active", animate( "3000ms" ))
+       ]),
+       trigger("fadeInOut", [
+      state("open", style({opacity: 1})),
+      state("closed", style({opacity: 0})),
+      transition("open <=> closed", animate( "3000ms" )),
+    ])
+       
+       ]
+      
+  
 })
 export class AppComponent implements OnInit {
   isDarkTheme= false;
   value = false;
-  side = 'start';
+  state = 'inactive';
+  side = 'open';
   
   ngOnInit(): void {
   }
@@ -36,5 +57,17 @@ export class AppComponent implements OnInit {
       window.location.reload();
     }
   }  
-  
+
+   beginAnim() {
+     // toggle state to kickstart animation
+      this.state = this.state === 'open' ? 'closed' : 'open';
+  }
+
+  toggleSidebar(value){
+    this.value = value;
+    this.state = 'inactive'
+    if(this.value){
+     this.state = 'active'
+    }
+}
 }
